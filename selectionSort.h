ce7428d9ca1e49466a1f64d573834be38e121734
TheDankLord <logan.hibbard@smail.astate.edu>
2017-10-14 16:01:17 -0500

    Update selectionSort.h

--- selectionSort.h
+++ selectionSort.h
@@ -1,3 +1,9 @@
+/*
+*   Aight bois, this one's simple. It should sort an integer vector passed to it, i.e:
+*               selectionsort(vector); <-- should work
+*
+*
+*/
 #ifndef SELECTIONSORT_H
 #define SELECTIONSORT_H
 
@@ -19,13 +25,15 @@ void swap(int& a, int& b){
 void selectionsort(vector<int>& v){
     int i, j;
     for(int j = 0; j < v.size()-1; ++j){
+        //assume first value is the minimum
         int min = j;
+        //run through array to verify
         for(i = j+1; i < v.size(); ++i){
             if(v[i] < v[min]){
                 min = i;
             }
         }
-    
+        //perform swap if another minimum value is found
         if(min != j){
             swap(v[j], v[min]);
         }

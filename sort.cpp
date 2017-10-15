49009d22d9a88240f962ded563f3aa83a1acf0a8
KrakenSociety <KrakenSociety@users.noreply.github.com>
2017-10-15 13:39:54 -0500

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -8,8 +8,11 @@ using namespace std;
 #include<vector>
     using std::vector;
 #include<algorithm>
-//including the quicksort algorithm
+//including the sorting files
 #include "quicksort.h"
+#include "mergesort.h"
+//#include "selectionsort.h"
+
 
 
 //prototype function
@@ -17,7 +20,7 @@ void printArray(int arr[], int size);
 void increase (vector<int> v, int size);
 void decrease (vector<int> v, int size);
 void random   (vector<int> v, int size);
-void phil_x   (vector<int> a1, vector<int> a2, vector<int> a3, int size);
+void fill_x   (vector<int> a1, vector<int> a2, vector<int> a3, int size);
 
 
 int main(){
@@ -64,144 +67,109 @@ int main(){
         }
         else if(x == 5){
             cout << "RUNNING MERGE SORT\n";
-        }
-        else if(x == 6){
-            cout << "RUNNING QUICK SORT\n";
-            int pass = 1;
-            clock_t t1, t2;
-            int count = 0;
-            int size;
+            int pass = 1;  //creating number of passes to know when to increment the size of the vector
+            clock_t t1, t2; //creating time variables to time the algorithms
+            int count = 0;  //
+            int size  = 0; //creating size variable to keep track of what size of vector we're working with
             while(pass < 5){
+                //populate 3 different vectors of the same size
+                //one in increasing order, one in decreaing order
+                //and one randomly respectively
                 if(pass == 1){
-                    phil_x(v1,v2,v3,size1);
+                    fill_x(v1,v2,v3,size1); 
                     size = size1;
                 }
                 else if(pass == 2){
-                    phil_x(v1,v2,v3,size2);
+                    fill_x(v1,v2,v3,size2);
                     size = size2;
                 }
                 else if(pass == 3){
-                    phil_x(v1,v2,v3,size3);
+                    fill_x(v1,v2,v3,size3);
                     size = size3;
                 }
                 else{
-                    phil_x(v1,v2,v3,size4);
+                    fill_x(v1,v2,v3,size4);
+                    size = size4;
+                }
+                    t1 = clock();
+                    mergesort(v1);    //all values in indexes are between 1 and 100
+                    t2 = clock();
+                    cout << "for " << size << " values increasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
+                         << " with " << count << " comparisons." << endl;
+                    count = 0;
+                    double T100_1 = (t2 - t1) / CLOCKS_PER_SEC;
+                     t1 = clock();
+                    mergesort(v2);    //all values in indexes are between 1 and 100
+                    t2 = clock();
+                    cout << "for " << size << " values  decreasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
+                         << " with " << count << " comparisons." << endl;
+                    count = 0;
+                    double T100_2 = (t2 - t1) / CLOCKS_PER_SEC;
+                    t1 = clock();
+                    mergesort(v3);    //all values in indexes are between 1 and 100
+                    t2 = clock();
+                    cout << "for " << size << " values  randomized the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
+                         << " with " << count << " comparisons." << endl;
+                    count = 0;
+                    double T100_3 = (t2 - t1) / CLOCKS_PER_SEC;
+                    cout << "average time for this pass: " << (T100_1 + T100_2 + T100_3) / 3 << endl;
+                ++pass;
+            }
+        }
+        else if(x == 6){
+            cout << "RUNNING QUICK SORT\n";
+            int pass = 1;  //creating number of passes to know when to increment the size of the vector
+            clock_t t1, t2; //creating time variables to time the algorithms
+            int count = 0;  //number of comparisons
+            int size  = 0;
+            while(pass < 5){
+                if(pass == 1){
+                    fill_x(v1,v2,v3,size1);
+                    size = size1;
+                }
+                else if(pass == 2){
+                    fill_x(v1,v2,v3,size2);
+                    size = size2;
+                }
+                else if(pass == 3){
+                    fill_x(v1,v2,v3,size3);
+                    size = size3;
+                }
+                else{
+                    fill_x(v1,v2,v3,size4);
                     size = size4;
                 }
                     t1 = clock();
                     quicksort(v1,0,size,count);    //100 values
                     t2 = clock();
-                    cout << "for" << size << " values increasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
+                    cout << "for " << size << " values increasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
                          << " with " << count << " comparisons." << endl;
                     count = 0;
                     
                      t1 = clock();
                     quicksort(v2,0,size,count);    //100 values
                     t2 = clock();
-                    cout << "for" << size << " values  decreasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
+                    cout << "for " << size << " values  decreasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
                          << " with " << count << " comparisons." << endl;
                     count = 0;
                     
                      t1 = clock();
                     quicksort(v3,0,size,count);    //100 values
                     t2 = clock();
-                    cout << "for" << size << " values  randomized the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
+                    cout << "for " << size << " values  randomized the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
                          << " with " << count << " comparisons." << endl;
                     count = 0;
-                }
-                {
-                //else if(pass == 2){
-                //    phil_x(v1,v2,v3,size2);
-                //    t1 = clock();
-                //    quicksort(v1,0,1000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 1000 values increasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //    
-                //     t1 = clock();
-                //    quicksort(v2,0,1000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 1000 values decreasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //    
-                //     t1 = clock();
-                //    quicksort(v3,0,1000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 1000 values randomized the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //}
-                //else if(pass == 3){
-                //    phil_x(v1,v2,v3,size3);
-                //    t1 = clock();
-                //    quicksort(v1,0,10000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 10000 values increasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //    
-                //     t1 = clock();
-                //    quicksort(v2,0,10000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 10000 values decreasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //    
-                //     t1 = clock();
-                //    quicksort(v3,0,10000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 10000 values randomized the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //}
-                //else if(pass == 4){
-                //    phil_x(v1,v2,v3,size4);
-                //    t1 = clock();
-                //    quicksort(v1,0,30000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 30000 values increasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //    
-                //     t1 = clock();
-                //    quicksort(v2,0,30000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 30000 values decreasing the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //    
-                //     t1 = clock();
-                //    quicksort(v3,0,30000,count);    //100 values
-                //    t2 = clock();
-                //    cout << "for 30000 values randomized the time is: " << ((t2 - t1)/CLOCKS_PER_SEC)
-                //         << " with " << count << " comparisons." << endl;
-                //    count = 0;
-                //}
-                }
                 ++pass;
-                ++size;
             }
-        //}
-        //else if(x == 7){
-        //    cout << "RUNNING RANDOMIZED QUICK\n";
-        //
-        //}
-        //else if(x == 8){
-        //    cout << "RUNNING HEAP SORT\n";
-        //}
-        //else{
-        //    cout << "Please pick a valid option\n";
-        //}
+        }
     }
     return 0;
 }
 
 
-void printArray(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        cout << "[" << arr[i] << "]  ";
+void printVector(vector<int> v){
+    for(int i = 0; i < v.size(); i++){
+        cout << "[" << v[i] << "]  ";
         if((i % 10) == 0){              //prints 10 before moving to a new line
             cout << endl;
         }
@@ -211,8 +179,9 @@ void printArray(int arr[], int size){
 
 
 void increase (vector<int> v, int size){
+    cout << "size: " << size << "\n";
     for (int i = 0; i < size; ++i){
-        v[i] = i;
+        v.push_back(i+1);
     }
     return;
 }
@@ -227,7 +196,7 @@ void random(vector<int> v, int size){
     for (int i = 0; i < size; ++i){
         srand(time(NULL));
         int randNum = rand() % 100 + 1;
-        v[i] = randNum;
+        v.push_back(randNum);
     }
     return;
 }
@@ -236,9 +205,10 @@ void random(vector<int> v, int size){
 /**
  * filling the vectors with inceasing, decreasing, and random values
  */
-void phil_x(vector<int> a1, vector<int> a2, vector<int> a3, int size){
+void fill_x(vector<int> a1, vector<int> a2, vector<int> a3, int size){
     increase(a1, size);
     decrease(a2, size);
     random(a3, size);
+    cout << "fill_x\n";
     return;
 }

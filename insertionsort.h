3051fa047bd067f9df64ff199dc446b66d4f2f0b
KrakenSociety <KrakenSociety@users.noreply.github.com>
2017-10-15 14:58:35 -0500

    Create insertionsort.h

---
+++ insertionsort.h
@@ -0,0 +1,26 @@
+#ifndef INSERTIONSORT_H
+#define INSERTIONSORT_H
+#include<iostream>
+#include<vector>
+   using std::vector;
+
+/*
+ *    sorts upon insert. just give it the vector and the count
+ *    and you should be good
+ */
+
+void insertionsort(vector<int> v, int count){
+   int i, key, j;
+   for (i = 1; i < v.size(); ++i){
+       key = v[i];
+       j = i-1;
+       while (j >= 0 && v[j] > key){
+           v[j+1] = v[j];
+           j = j-1;
+           ++count;
+       }
+       v[j+1] = key;
+   }
+}
+
+#endif

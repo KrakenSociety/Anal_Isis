0a2a0efb8b10457e8ae644bd064edd054e21dd79
TheDankLord <logan.hibbard@smail.astate.edu>
2017-10-15 19:04:17 -0500

    Update selectionSort.h

--- selectionSort.h
+++ selectionSort.h
@@ -21,18 +21,23 @@
 
 
     
-void selectionsort(vector<int>& v){
+void selectionsort(vector<int>& v, int& count){
     int i, j;
+    count = 0;
     for(int j = 0; j < v.size()-1; ++j){
+        count++;// for oop ^above
         //assume first value is the minimum
         int min = j;
         //run through array to verify
+        count++;//for loop below
         for(i = j+1; i < v.size(); ++i){
+            count++;//for if below
             if(v[i] < v[min]){
                 min = i;
             }
         }
         //perform swap if another minimum value is found
+        count++;//for if below
         if(min != j){
             std::swap(v[j], v[min]);
         }

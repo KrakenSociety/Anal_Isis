85461f639e2fd54ada2a2288e8298a852e85382c
sllauQ <32281574+sllauQ@users.noreply.github.com>
2017-10-15 21:00:46 -0500

    adding bubble sort header

---
+++ bubblesort.h
@@ -0,0 +1,49 @@
+/*
+* just a bubble sort
+*/
+
+
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+#include <iostream>
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    
+#include <cstdlib>
+#include <vector>
+    using std::vector;
+    
+int bubblesort(vector<int> a)
+{
+    int size = a.size();
+    int temp;
+    int count = 0;
+    while(size > 0)
+    {
+        count++;
+        for(int j = 0; j < size - 1; j++)
+        {
+            count++;
+            if(a[j] > a[j+1])
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+        size--;
+        count++;
+    }
+    count++;
+    /*
+    cout << "printing vector:\n";
+    for(int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << endl;
+    }
+    cout << endl;*/
+    return count;
+}
+#endif

4cfb903c839e8aedc2f36a0d5a1494da922a7df6
sllauQ <32281574+sllauQ@users.noreply.github.com>
2017-10-16 09:41:20 -0500

    deleted unused mod

--- modinsertionsort.h
+++ modinsertionsort.h
@@ -8,69 +8,6 @@
  *    modified insertion sort with binary search instead of linear search
  */
 
-//void modinsertionsort(vector<int>& v, int& count)
-//{
-//    int i , key,  j, size, index;
-//    //int count = 0;
-//    count++;
-//    for(i = 1; i < v.size(); ++i)
-//    {
-//        size = v.size();
-//        index = size / 2;
-//        size = size / 2;
-//        key = v[index];
-//        j = i - 1;
-//        count++;
-//        while (j >= 0)
-//        {
-//            count++;
-//            if(v[j] >= key)
-//            {
-//                count++;
-//                if(size != 1)
-//                {
-//                    index = size - size /2;
-//                    size /= 2;
-//                    key = v[index];
-//                }
-//                else
-//                {
-//                    index -= 1;
-//                    key = v[index];
-//                    v[index] = v[j];
-//                    v[j] = key;
-//                    j = -1;
-//                }
-//            }
-//            else
-//            {
-//                count++;
-//                if(size != 1)
-//                {
-//                    index = size + size / 2;
-//                    size /= 2;
-//                    key = v[index];
-//                }
-//               else
-//                {
-//                    index += 1;
-//                    key = v[index];
-//                    v[index] = v[j];
-//                    v[j] = key;
-//                    j = -1;
-//                }
-//            }
-//        }
-//    }
-////     for(int i = 0; i < v.size(); i++)
-////     {
-////         cout << v[i] << endl;
-////     }
-
-//   return;
-//}
- 
-
 
 int binarySearch(vector<int>& v, int item, int low, int high)
 {
